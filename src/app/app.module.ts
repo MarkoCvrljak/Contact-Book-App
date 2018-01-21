bb13f2f176780e421451f369e366c558562733ba
Marko Čvrljak <marko.cvrljak1@gmail.com>
2018-01-21 11:34:39 +0100

    creating basic component folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ContactsListComponent } from './contacts/contacts-list/contacts-list.component';
+import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
+import { ContactsItemComponent } from './contacts/contacts-list/contacts-item/contacts-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContactsComponent
+    ContactsComponent,
+    ContactsListComponent,
+    ContactsDetailComponent,
+    ContactsItemComponent
   ],
   imports: [
     BrowserModule
