c07ec45446ccbfd8754a9282dbf085f47a9c40fe
Marko Čvrljak <marko.cvrljak1@gmail.com>
2018-01-21 11:21:13 +0100

    creating contacts component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
